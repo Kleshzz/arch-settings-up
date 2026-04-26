85eaf4f818c66bcba1abe97138283ab9d1f310b9
Kleshzz <vasyapulopapik@gmail.com>
2026-04-26 11:02:30 +0300

    update configs

--- firefox/user.js
+++ firefox/user.js
@@ -72,7 +72,7 @@ user_pref("full-screen-api.warning.timeout", 0);
 
 // Fast work
 user_pref("gfx.content.skia-font-cache-size", 32);
-user_pref("dom.ipc.processCount", 8);
+// user_pref("dom.ipc.processCount", 8); // firefox сам выбирает оптимал
 user_pref("browser.cache.memory.capacity", 262144); // 256MB под кеш в RAM
 user_pref("browser.cache.memory.max_entry_size", 51200); // макс 50MB на элемент
 user_pref("browser.sessionhistory.max_total_viewers", 3);
