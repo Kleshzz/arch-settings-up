9bd6a0aaf62564d74da543fd46a9904e6087e470
Kleshzz <vasyapulopapik@gmail.com>
2026-04-27 16:09:50 +0300

    update configs

--- dotfiles/.config/dunst/dunst-sound.sh
+++ dotfiles/.config/dunst/dunst-sound.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/bin/paplay /usr/share/sounds/freedesktop/stereo/message.oga
+if [ "$DUNST_APP_NAME" != "Hyprshot" ]; then
+    /usr/bin/paplay /usr/share/sounds/freedesktop/stereo/message.oga
+fi
